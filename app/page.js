3ce28d8794cf94f0dd4cbfb1e485e63e5f02847f
sahebbali <saheb1996jmr@gmail.com>
2024-08-02 08:34:08 +0600

    initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-red-900 c">
+        <h1>Hello </h1>
+      </div>
+    </>
+  );
+}
