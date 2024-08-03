c09ba89f5a865b8d89fe3e8e437a701f37dd56ea
sahebbali <saheb1996jmr@gmail.com>
2024-08-03 09:30:07 +0600

    add signin with google button

--- app/(website)/login/page.js
+++ app/(website)/login/page.js
@@ -1,12 +1,10 @@
-import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
+import LoginWithGoogle from "@/app/components/buttons/LoginWithGoogle";
 
 export default function LoginPage() {
   return (
     <div>
       <div className="p-4 max-w-xs mx-auto">
-        <h1 className="text-4xl font-bold text-center mb-2">
-          Sign In
-        </h1>
+        <h1 className="text-4xl font-bold text-center mb-2">Sign In</h1>
         <p className="text-center mb-6 text-gray-500">
           Sign in to your account using one of the methods below
         </p>
