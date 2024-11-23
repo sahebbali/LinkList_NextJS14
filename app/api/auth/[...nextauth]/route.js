fe727f7680b9082b94dafe160eea7be888726223
sahebbali <saheb1996jmr@gmail.com>
2024-11-23 09:37:58 +0600

    fixed google auth issue

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,2 @@
-import clientPromise from "@/libs/mongoClient";
-import {MongoDBAdapter} from "@auth/mongodb-adapter";
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
-  ],
-};
-
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
+import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+export const { GET, POST } = handlers;
--- app/components/buttons/LoginWithGoogle.js
+++ app/components/buttons/LoginWithGoogle.js
@@ -1,15 +1,23 @@
 "use client";
-// import {faGoogle} from "@fortawesome/free-brands-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { FaGoogle } from "react-icons/fa";
 import { signIn } from "next-auth/react";
 
 export default function LoginWithGoogle() {
+  const handleSignIn = async () => {
+    try {
+      await signIn("google");
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
   return (
     <button
-      onClick={() => signIn("google")}
-      className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center"
+      onClick={handleSignIn}
+      className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center hover:bg-gray-100 transition"
     >
-      {/* <FontAwesomeIcon icon={faGoogle} className="h-6" /> */}
+      <FaGoogle className="h-6" />
       <span>Sign In with Google</span>
     </button>
   );
