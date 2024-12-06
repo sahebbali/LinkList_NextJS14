fb7d5608061ac61cea7111d5a0adba3bcae978cb
sahebbali <saheb1996jmr@gmail.com>
2024-12-06 11:27:26 +0600

    add line charte

--- app/(app)/analytics/page.js
+++ app/(app)/analytics/page.js
@@ -1,99 +1,119 @@
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
-// import Chart from "@/components/Chart";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-import {Event} from "@/models/Event";
-import {Page} from "@/models/Page";
-import { FaLink,
-} from "react-icons/fa";
+import { Event } from "@/models/Event";
+import { Page } from "@/models/Page";
+import { FaLink } from "react-icons/fa";
 
-import {differenceInDays, formatISO9075, isToday} from "date-fns";
+import { differenceInDays, formatISO9075, isToday } from "date-fns";
 import mongoose from "mongoose";
-import {getServerSession} from "next-auth";
-import {redirect} from "next/navigation";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import SectionBox from "@/app/components/layout/SectionBox";
-// import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
-
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import Chart from "@/app/components/Chart";
 
 export default async function AnalyticsPage() {
   mongoose.connect(process.env.MONGO_URI);
   const session = await getServerSession(authOptions);
   if (!session) {
-    return redirect('/');
+    return redirect("/");
   }
-  const page = await Page.findOne({owner: session.user.email});
+  const page = await Page.findOne({ owner: session.user.email });
 
   const groupedViews = await Event.aggregate([
     {
       $match: {
-        type: 'view',
+        type: "view",
         uri: page.uri,
-      }
+      },
     },
     {
       $group: {
         _id: {
           $dateToString: {
             date: "$createdAt",
-            format: "%Y-%m-%d"
+            format: "%Y-%m-%d",
           },
         },
         count: {
-          "$count": {},
-        }
+          $count: {},
+        },
       },
     },
     {
-      $sort: {_id: 1}
-    }
+      $sort: { _id: 1 },
+    },
   ]);
 
   const clicks = await Event.find({
     page: page.uri,
-    type: 'click',
+    type: "click",
   });
 
   return (
     <div>
-      {/* <SectionBox>
+      <SectionBox>
         <h2 className="text-xl mb-6 text-center">Views</h2>
-        <Chart data={groupedViews.map(o => ({
-          'date': o._id,
-          'views': o.count,
-        }))} />
-      </SectionBox> */}
+        <Chart
+          data={groupedViews.map((o) => ({
+            date: o._id,
+            views: o.count,
+          }))}
+        />
+      </SectionBox>
       <SectionBox>
         <h2 className="text-xl mb-6 text-center">Clicks</h2>
-        {page.links.map(link => (
-          <div key={link.title} className="md:flex gap-4 items-center border-t border-gray-200 py-4">
+        {page.links.map((link) => (
+          <div
+            key={link.title}
+            className="md:flex gap-4 items-center border-t border-gray-200 py-4"
+          >
             <div className="text-blue-500 pl-4">
               <FaLink />
             </div>
             <div className="grow">
-              <h3>{link.title || 'no title'}</h3>
-              <p className="text-gray-700 text-sm">{link.subtitle || 'no description'}</p>
-              <a className="text-xs text-blue-400" target="_blank" href="link.url">{link.url}</a>
+              <h3>{link.title || "no title"}</h3>
+              <p className="text-gray-700 text-sm">
+                {link.subtitle || "no description"}
+              </p>
+              <a
+                className="text-xs text-blue-400"
+                target="_blank"
+                href="link.url"
+              >
+                {link.url}
+              </a>
             </div>
             <div className="text-center">
               <div className="border rounded-md p-2 mt-1 md:mt-0">
                 <div className="text-3xl">
                   {
-                    clicks
-                      .filter(
-                        c => c.uri === link.url
-                          && isToday(c.createdAt)
-                      )
-                      .length
+                    clicks.filter(
+                      (c) => c.uri === link.url && isToday(c.createdAt)
+                    ).length
                   }
                 </div>
-                <div className="text-gray-400 text-xs uppercase font-bold">clicks today</div>
+                <div className="text-gray-400 text-xs uppercase font-bold">
+                  clicks today
+                </div>
               </div>
             </div>
             <div className="text-center">
               <div className="border rounded-md p-2 mt-1 md:mt-0">
                 <div className="text-3xl">
-                  {clicks.filter(c => c.uri === link.url).length}
+                  {clicks.filter((c) => c.uri === link.url).length}
                 </div>
-                <div className="text-gray-400 text-xs uppercase font-bold">clicks total</div>
+                <div className="text-gray-400 text-xs uppercase font-bold">
+                  clicks total
+                </div>
               </div>
             </div>
           </div>
